a97c2178741d6c1e32017c1adcfac9b39bbba3ba
Ayush Pandey <baku@Bakus-Mac.local>
2024-06-23 00:25:29 +0545

    initial setup and hero serction done

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Hero from "./_components/Hero";
+import CategorySearch from "./_components/CategorySearch";
 
 export default function Home() {
   return (
     <div>
-      <h2>Daktarsahab</h2>
-      <Button >Button</Button>
+      <Hero />
+      <CategorySearch />
     </div>
   );
 }
