a0237c48802d442a9af37afa999d21b229195882
rakshya682 <rakshya.basnet@apexcollege.edu.np>
2024-06-25 07:51:36 +0545

    authentication

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,7 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { LoginLink, LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+
 
 function Header() {
   const Menu = [
@@ -21,14 +30,20 @@ function Header() {
       path: "/",
     },
   ];
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <div className="flex items-center justify-between p-4 shadow-sm">
       <div className="flex items-center gap-10">
         <Image src="/logo.svg" alt="logo" width={180} height={80} />
 
         <ul className="md:flex gap-8 hidden">
-          {Menu.map((item, index) => (
-            <Link href={item.path}>
+          {Menu.map((item) => (
+            <Link href={item.path} key={item.id}>
               <li className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out">
                 {item.name}
               </li>
@@ -36,7 +51,36 @@ function Header() {
           ))}
         </ul>
       </div>
-      <Button>Get Started</Button>
+      {user ? 
+        
+        <Popover>
+        <PopoverTrigger><Image src={user?.picture} alt='profile-image'
+        width={50}
+        height={50}
+        className="rounded-full"/></PopoverTrigger>
+        <PopoverContent className='w-44'>
+          <ul className='flex flex-col gap-2'>
+            <li className="cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md">
+              Profile
+            </li>
+            <li lassName="cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md">
+                My Bookings           
+                 </li>
+            <li lassName="cursor-pointer
+             hover:bg-slate-100 p-2 rounded-md">
+              <LogoutLink><Button variant="outline">Log out</Button>
+         </LogoutLink>
+            </li>
+          </ul>
+        </PopoverContent>
+      </Popover>
+       : 
+        <LoginLink>
+          <Button>Get Started</Button>
+        </LoginLink>
+      }
     </div>
   );
 }
